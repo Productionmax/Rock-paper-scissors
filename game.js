9f80ffa64301f59192cfd930c3fed4c61267ac8d
Jonathan Killen <Productionmax@hotmail.com>
2022-07-25 18:39:55 +0800

    Writing a function that plays a single round of Rock Paper Scissors by taking the parameters of the player's selection and the computer's selection and decides the winner by using IF statements

--- game.js
+++ game.js
@@ -1,7 +1,32 @@
 //Set the variables for the three different choices of the game.
-let choices = ['rock', 'paper', 'scissors'];
+let choices = ['rock', 'paper', 'scissor'];
 //Made a function so that the computer would randomly return a choice.
 function getComputerChoice(){
    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
    return computerChoice;
 }
+
+function round1(playerSelection, computerSelection){
+    if (playerSelection===computerSelection){
+        return "It's a tie!You both chose ${playerSelection} ";
+    }  else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lose! Paper beats Rock";
+    }  else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You Win! Paper beats Rock";
+    }  else if (playerSelection === "scissor" && computerSelection === "paper"){
+        return "You Win! Scissor beats Rock";
+    }  else if (playerSelection === "paper" && computerSelection === "scissor"){
+        return "You Lose! Scissor beats rock";
+    }  else if (playerSelection === "rock" && computerSelection === "scissor"){
+        return "You win! Rock beats Scissor";
+    }  else if (playerSelection === "scissor" && computerSelection === "rock"){
+        return "You Lose! Rock beats Scissor";
+    }  else if (playerSelection === "scissor" && computerSelection === "paper"){
+        return "You Win! Scissor beats Paper";
+    }  else if (playerSelection === "paper" && computerSelection === "scissor"){
+        return "You Lose! Scissor beats Paper";
+    }  else {return "You need to choose either Rock, Paper, Scissors!";}
+}
+console.log(round1("scissor",getComputerChoice()))
+
+
