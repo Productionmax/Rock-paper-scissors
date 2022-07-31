ac2339079d1076eacceca1adb75ed3fd9c5085df
Jonathan Killen <Productionmax@hotmail.com>
2022-07-31 15:13:32 +0800

    Added CSS to the buttons so that they will lose their background

--- game.js
+++ game.js
@@ -40,21 +40,5 @@ paper.addEventListener('click',function(){
 scissor.addEventListener('click',function(){
     playRound('scissor',getComputerChoice());
 });
-// A function that counts the score and plays the game for 5 rounds 
-// function game(){
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i=0;i<5; i++){
-//         const playerSelection = prompt("What's your choice?").toLowerCase();
-//         const computerSelection = getComputerChoice();
-//         result = playRound(playerSelection,computerSelection);
-//         console.log(result);
-//         if (result === "You Win! Paper beats Rock" || result === "You Win! Scissor beats Rock"||result === "You win! Rock beats Scissor" || result === "You Win! Scissor beats Paper" ){
-//             playerScore++; 
-//         } else {computerScore++;}
-//     }
-//     console.log(playerScore);
-//     console.log(computerScore);
-// }
-//console.log(game());
+
 
